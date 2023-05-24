091d75073c3b1cd4a3c2e0e7b0a95f84281184b0
sam <shoaibur206@gmail.com>
2023-05-24 12:16:35 +0500

    add firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,20 @@
-import * as firebase from "firebase"
-import "firebase/auth"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
-const app = firebase.initializeApp({
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.FIREBASE_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.SENDER_ID,
-    appId: process.env.APP_ID
-});
+const firebaseConfig = {
+    apiKey:'AIzaSyBO9R-8MxePaIJ1tUZV7TCyEj1ALEnPWXo',
+    authDomain: 'react-firebase-auth-5fc2f.firebaseapp.com',
+    projectId: 'react-firebase-auth-5fc2f',
+    storageBucket: 'react-firebase-auth-5fc2f.appspot.com',
+    messagingSenderId: '615211616467',
+    appId:'1:615211616467:web:026bba90edf26f64a8ac26'
+};
 
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
 export default app;
